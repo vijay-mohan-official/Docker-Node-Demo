7c49c5463fc1f72975226d4195662dce736e4454
vijay-mohan-official <vijaymohanofficial@gmail.com>
2024-08-24 10:19:25 +0530

    Pushing codebase to git

---
+++ app/server.js
@@ -0,0 +1,85 @@
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+const MongoClient = require('mongodb').MongoClient;
+const bodyParser = require('body-parser');
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+const mongoUrlLocal = "mongodb://admin:password@localhost:27017";
+const mongoUrlDocker = "mongodb://admin:password@mongodb";
+const mongoClientOptions = { useNewUrlParser: true, useUnifiedTopology: true };
+const databaseName = "my-db";
+
+let dbClient;
+
+// Connect to MongoDB once and reuse the client
+MongoClient.connect(mongoUrlDocker, mongoClientOptions)
+  .then(client => {
+    dbClient = client;
+    console.log('Connected to MongoDB');
+  })
+  .catch(err => {
+    console.error('Failed to connect to MongoDB', err);
+  });
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.get('/profile-picture', (req, res) => {
+  fs.readFile(path.join(__dirname, 'images/profile-1.jpg'), (err, img) => {
+    if (err) {
+      res.status(500).send('Error reading image file');
+      return;
+    }
+    res.writeHead(200, { 'Content-Type': 'image/jpg' });
+    res.end(img, 'binary');
+  });
+});
+
+app.post('/update-profile', (req, res) => {
+  const userObj = req.body;
+  userObj['userid'] = 1;
+
+  if (!dbClient) {
+    res.status(500).send('Database not connected');
+    return;
+  }
+
+  const db = dbClient.db(databaseName);
+  const myquery = { userid: 1 };
+  const newvalues = { $set: userObj };
+
+  db.collection('users').updateOne(myquery, newvalues, { upsert: true }, (err, result) => {
+    if (err) {
+      res.status(500).send('Error updating profile');
+      return;
+    }
+    res.send(userObj);
+  });
+});
+
+app.get('/get-profile', (req, res) => {
+  if (!dbClient) {
+    res.status(500).send('Database not connected');
+    return;
+  }
+
+  const db = dbClient.db(databaseName);
+  const myquery = { userid: 1 };
+
+  db.collection('users').findOne(myquery, (err, result) => {
+    if (err) {
+      res.status(500).send('Error fetching profile');
+      return;
+    }
+    res.send(result || {});
+  });
+});
+
+app.listen(3000, () => {
+  console.log('App listening on port 3000!');
+});
